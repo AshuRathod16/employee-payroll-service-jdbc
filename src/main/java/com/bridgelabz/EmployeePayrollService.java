1e0831ac6a4e21516ec7279dfe1197267f628735
Qa-krushna <rashwini510@gmail.com>
2022-07-26 14:29:04 +0530

    Added: Ability to update salary data using prepared statement

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -10,13 +10,20 @@ public class EmployeePayrollService {
         String userName = "root";
         String password = "password";
         Connection connection = null;
+        double basicPay = 400000.00;
+        int id = 1;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded!");
             connection = DriverManager.getConnection(jdbcURL, userName, password);
             System.out.println("Connection done!!!");
+            String query = "UPDATE employee_payroll SET basicPay=? WHERE id=?";
+            PreparedStatement prestatement = connection.prepareStatement(query);
+            prestatement.setDouble(1, basicPay);
+            prestatement.setInt(2, id);
+            prestatement.executeUpdate();
+
             Statement statement = connection.createStatement();
-            statement.execute("UPDATE employee_payroll SET BasicPay=3000000.00 WHERE id=1");
             ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_payroll");
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt("id") + " | " + resultSet.getString("name") + " | " + resultSet.getString("gender") + " | " + resultSet.getString("phone_number") + " | " + resultSet.getString("address") + " | " + resultSet.getString("department") + " | " + resultSet.getDouble("basicPay") + " | " + resultSet.getDouble("deductions") + " | " + resultSet.getDouble("taxablePay") + " | " + resultSet.getDouble("netPay") + " | " + resultSet.getDouble("incomeTax") + " | " + resultSet.getDate("start"));
